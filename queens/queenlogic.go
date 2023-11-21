ddd137d5b4a7cfe42157b568bbee1096b99ee384
Reiner Hüchting <dev@reinerhuechting.de>
2023-11-22 00:51:56 +0100

    Remove code tags.

--- queens/queenlogic.go
+++ queens/queenlogic.go
@@ -5,60 +5,39 @@ import "github.com/wwi23ama-prog/schachprobleme/board"
 // RowAllowed erwartet ein Spielfeld und eine Zeilennummer.
 // Die Funktion liefert true, falls in dieser Zeile noch eine Dame gesetzt werden darf.
 func RowAllowed(b board.Board, row int) bool {
-	// begin:hint
 	// Benutzen Sie die Funktion RowContainsOnly aus dem Paket board.
 	// Prüfen Sie, ob in der Zeile nur Leerzeichen vorkommen.
-	// end:hint
-	// begin:solution
 	return b.RowContainsOnly(row, " ")
-	// end:solution
-	// iftask: return false
 }
 
 // ColAllowed erwartet ein Spielfeld und eine Spaltennummer.
 // Die Funktion liefert true, falls in dieser Spalte noch eine Dame gesetzt werden darf.
 func ColAllowed(b board.Board, col int) bool {
-	// begin:hint
 	// Benutzen Sie die Funktion ColContainsOnly aus dem Paket board.
-	// end:hint
-	// begin:solution
 	return b.ColContainsOnly(col, " ")
-	// end:solution
-	// iftask: return false
 }
 
 // DiagRightAllowed erwartet ein Spielfeld und Koordinaten.
 // Die Funktion liefert true, falls in der Diagonalen, die durch dieses Feld
 // von links oben nach rechts unten verläuft, noch eine Dame gesetzt werden darf.
 func DiagRightAllowed(b board.Board, row, col int) bool {
-	// begin:hint
 	// Benutzen Sie die Funktion DiagRightContainsOnly aus dem Paket board.
 	// Diese Funktion erwartet die Spalte, in der diese Diagonale ganz oben
 	// beginnt. Diese Spalte müssen Sie hier berechnen.
-	// end:hint
-	// begin:solution
 	return b.DiagRightContainsOnly(col-row, " ")
-	// end:solution
-	// iftask: return false
 }
 
 // DiagLeftAllowed erwartet ein Spielfeld und Koordinaten.
 // Die Funktion liefert true, falls in der Diagonalen, die durch dieses Feld
 // von rechts oben nach links unten verläuft, noch eine Dame gesetzt werden darf.
 func DiagLeftAllowed(b board.Board, row, col int) bool {
-	// begin:hint
 	// Benutzen Sie die Funktion DiagLeftContainsOnly aus dem Paket board.
-	// end:hint
-	// begin:solution
 	return b.DiagLeftContainsOnly(col+row, " ")
-	// end:solution
-	// iftask: return false
 }
 
 // QueenAllowed erwartet ein Spielfeld und Koordinaten.
 // Die Funktion liefert true, falls an diese Stelle noch eine Dame gesetzt werden darf.
 func QueenAllowed(b board.Board, row, col int) bool {
-	// begin:hint
 	// Eine Dame darf gesetzt werden, wenn die Zeile,
 	// die Spalte und beide Diagonalen frei sind.
 	return RowAllowed(b, row) &&
--- queens/solvequeens.go
+++ queens/solvequeens.go
@@ -7,7 +7,6 @@ import "github.com/wwi23ama-prog/schachprobleme/board"
 // dieses Spielfeld ab dieser Zeile zu lösen.
 // Sie liefert true zurück, falls das Problem gelöst werden konnte.
 func SolveQueensFromRow(b board.Board, row int) bool {
-	// begin:hint
 	// Rekursionsanker: Wenn die Zeile außerhalb des Spielfelds liegt,
 	// ist das Problem gelöst.
 	// Rekursionsschritt:
@@ -15,8 +14,6 @@ func SolveQueensFromRow(b board.Board, row int) bool {
 	// Setze eine Dame und versuche, das Problem ab der nächsten Zeile zu lösen.
 	// Gelingt dies, ist das Problem gelöst.
 	// Gelingt dies nicht, entferne die Dame wieder und versuche die nächste Spalte.
-	// end:hint
-	// begin:solution
 	if row >= b.Rows() {
 		return true
 	}
@@ -33,14 +30,12 @@ func SolveQueensFromRow(b board.Board, row int) bool {
 
 	// Wenn das Problem an dieser Stelle immer noch nicht gelöst wurde,
 	// ist es in der aktuellen Konfiguration gar nicht lösbar.
-	// end:solution
 	return false
 }
 
 // SolveQueens erwartet ein Spielfeld und löst das Damenproblem für dieses Spielfeld.
 // Die Funktion liefert true zurück, falls das Problem gelöst werden konnte.
 func SolveQueens(b board.Board) bool {
-	// begin:hint
 	// Die eigentliche Arbeit macht die Funktion SolveQueensFromRow.
 	// Sie muss mit der ersten Zeile aufgerufen werden.
 	//
@@ -49,6 +44,5 @@ func SolveQueens(b board.Board) bool {
 	// eine zweite Funktion, die die erste Funktion mit den richtigen
 	// Parametern aufruft, so dass diese Parameter bei der Benutzung
 	// der Funktion nicht mehr bekannt sein müssen.
-	// end:hint
 	return SolveQueensFromRow(b, 0)
 }
